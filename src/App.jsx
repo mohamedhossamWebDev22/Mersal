867db62fada72a5621be27968176953c4f5f4b3e
MohamedHossamElden <mohamedhossamelden2009@gmail.com>
2024-02-03 15:16:55 +0200

    Add Website

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import FormC from './components/form';
+
+import "./style.scss"
+
+const App = () => {
+  return (
+    <>
+      <FormC />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/form.jsx
@@ -0,0 +1,53 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+import logo from "../assets/logo.png"
+
+
+export const FormC = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_q', 'template_4to2kza', form.current, 'o-_IssFaqVS_8CuI6')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail}>
+      <div className="logo">
+        <h1>مرســــال</h1>
+        <img width="100px" src={logo} alt="Logo" />
+      </div>
+
+      <div className="p">
+        <div className="ch">
+          <label>To Name</label>
+          <input type="text" name="to_name" />
+          <br />
+          <label>To Email</label>
+          <input type="email" name="to_email" />
+        </div>
+
+        <div className="ch">
+          <label>From Name</label>
+          <input type="text" name="user_name" />
+          <br />
+          <label>From Email</label>
+          <input type="email" name="user_email" />
+        </div>
+      </div>
+      <label>Message</label>
+      <textarea name="message" />
+      <br />
+      <input type="submit" value="Send" />
+    </form>
+  );
+};
+
+export default FormC;
